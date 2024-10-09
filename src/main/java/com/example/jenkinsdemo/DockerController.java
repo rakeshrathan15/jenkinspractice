adf1d35040b82aa17951981fde876772ad3028c7
rakesh mekala <rakeshyadavmekala15@gmail.com>
2024-10-09 23:17:05 +0530

    jenkins practice1

--- src/main/java/com/example/jenkinsdemo/DockerController.java
+++ src/main/java/com/example/jenkinsdemo/DockerController.java
@@ -9,7 +9,7 @@ public class DockerController {
 
     @GetMapping(value = "/api/docker")
     public String getHelloWorld(){
-        return "Hello  jenkins";
+        return "Hello world jenkins";
     }
 
 }
